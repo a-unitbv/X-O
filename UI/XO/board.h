6c5d805be4f6f2503f362589f4fa5c7fd80825ad
dan <fiendanpines@yahoo.com>
2023-03-13 18:41:00 +0200

    uploaded QT implementation of the game

---
+++ UI/XO/board.h
@@ -0,0 +1,26 @@
+#ifndef BOARD_H
+#define BOARD_H
+
+#include <iostream>
+#include <iomanip>
+#include <ctime>
+
+class Board
+{
+private:
+    char board[3][3];
+    int occupiedPositionsCount = 0;
+    char turn;
+
+public:
+    Board();
+    void InitBoard();
+    void MarkAndChangeTurn(int row, int column);
+    char WhoGoesFirst();
+    char IsWinner();
+    char GetTurn();
+    int GetOccupiedPositionsCount();
+    bool IsAlreadyMarked(int row, int column);
+};
+
+#endif // BOARD_H
